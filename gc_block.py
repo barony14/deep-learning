389fe2a613d8772973cb9172ea11624a696fb7ce
barony14 <33677387+barony14@users.noreply.github.com>
2019-06-05 09:32:59 +0800

    Add files via upload

--- gc_block.py
+++ gc_block.py
@@ -1,35 +1,36 @@
 from keras.layers import *
-from keras.activations import relu
+from keras.activations import relu, softmax
 from keras_layer_normalization import LayerNormalization
 import tensorflow as tf
 
 
 def global_context_block(x):
+    """
+    高维矩阵乘法[n,1,c,hw]*[n,1,hw,1]=[n,1,c,1]
+    GC_block:global context block
+    :parameter x:input layers or tensor
+    """
+
     bs, h, w, c = x.get_shape().as_list()
     input_x = x
-    input_x = hw_flatten(input_x)  # [N, H*W, C]
-    input_x = tf.transpose(input_x, perm=[0, 2, 1])
-    input_x=tf.reshape(input_x,[-1,784])
-    #input_x = tf.expand_dims(input_x, axis=1)
+    input_x = Reshape((-1, c))(input_x)  # [N, H*W, C]
+    input_x = tf.transpose(input_x, perm=[0, 2, 1])  # [N,C,H*W]
+    input_x = tf.expand_dims(input_x, axis=1)
 
-    context_mask = Conv2D(filters=1,kernel_size=(1,1))(x)
-    context_mask = hw_flatten(context_mask)
-    context_mask = tf.nn.softmax(context_mask, axis=1)  # [N, H*W, 1]
-    context_mask = tf.transpose(context_mask, perm=[1,2,0])
-    context_mask=tf.reshape(context_mask,[784,-1])
-    #context_mask = tf.expand_dims(context_mask, axis=2)
+    context_mask = Conv2D(filters=1, kernel_size=(1, 1))(x)
+    context_mask = Reshape((-1, 1))(context_mask)
+    context_mask = softmax(context_mask, axis=1)  # [N, H*W, 1]
+    context_mask = tf.transpose(context_mask, [0, 2, 1])
+    context_mask = tf.expand_dims(context_mask, axis=-1)
 
-    context = tf.matmul(input_x, context_mask)
-    context = tf.reshape(context, [-1, 1, 1, c])
+    context = tf.matmul(input_x, context_mask)  # [N,1,c,1]
+    context = Reshape((1, 1, c))(context)
 
-    context_transform = Conv2D(c, (1,1))(context)
+    context_transform = Conv2D(int(c/8), (1, 1))(context)
     context_transform = LayerNormalization()(context_transform)
     context_transform = relu(context_transform)
-    context_transform = Conv2D(c,(1,1))(context_transform)
+    context_transform = Conv2D(c, (1, 1))(context_transform)
 
     x = x + context_transform
 
     return x
-
-def hw_flatten(x):
-    return tf.reshape(x, [-1, x.get_shape().as_list()[1]*x.get_shape().as_list()[2], x.get_shape().as_list()[3]])
